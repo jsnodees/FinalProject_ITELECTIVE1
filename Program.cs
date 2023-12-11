fb1a5a2d2be0891f640f187e6b8e9156e759ba0d
jsnodees <202280134@psu.palawan.edu.ph>
2023-12-11 10:51:10 +0800

    Added StudentMainMenu

--- Program.cs
+++ Program.cs
@@ -1,11 +1,60 @@
-public class Student
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
 {
-    public int student{ get; set; }
-    public string surname { get; set; }
-    public string firstname { get; set; }
-    public string occupation { get; set; }
-    public string gender { get; set; }
-    public int countrycode { get; set; }
-    public int areacode { get; set; }
-    public string phonenumber { get; set; }
-}
+
+
+    public class Student
+    {
+        public int student { get; set; }
+        public string surname { get; set; }
+        public string firstname { get; set; }
+        public string occupation { get; set; }
+        public string gender { get; set; }
+        public int countrycode { get; set; }
+        public int areacode { get; set; }
+        public string phonenumber { get; set; }
+
+    }
+
+    public class StudentMainMenu
+    {
+        private List<Student> studentsdatabase = new List<Student>();
+        public void Start()
+        {
+            while (true)
+            {
+                Console.WriteLine("Main Menu:");
+                Console.WriteLine("[1] Store to ASEAN phonebook");
+                Console.WriteLine("[2] Edit entry in ASEAN phonebook");
+                Console.WriteLine("[1] Search ASEAN phonebook by country");
+                Console.WriteLine("[1] Exit");
+
+                Console.WriteLine("Enter your choice: ");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        studentinfo();
+                        break;
+                    case "2":
+                        editinfo();
+                        break;
+                    case "3":
+                        searchinfo();
+                        break;
+                    case "4":
+                        Console.WriteLine("Exiting program.");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Try again.");
+                        break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
