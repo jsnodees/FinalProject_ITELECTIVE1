eb2272872c67b7b7f18122d62781cd91c211d871
jsnodees <202280134@psu.palawan.edu.ph>
2023-12-11 10:48:08 +0800

    Student

---
+++ Program.cs
@@ -0,0 +1,11 @@
+public class Student
+{
+    public int student{ get; set; }
+    public string surname { get; set; }
+    public string firstname { get; set; }
+    public string occupation { get; set; }
+    public string gender { get; set; }
+    public int countrycode { get; set; }
+    public int areacode { get; set; }
+    public string phonenumber { get; set; }
+}
